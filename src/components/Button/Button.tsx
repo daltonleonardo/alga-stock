54a51b92610f7bea6ade74a681ae3fdc5a553416
dalton.leonardo <daltonleonardo@hotmail.com>
2020-11-13 17:49:58 -0300

    add WithChildren helper type

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -8,22 +8,39 @@ import './Button.css';
 //tipagem direta
 // const Button: React.FC<{ content: string}>= (props) => {
 
-declare interface ButtonProps {
+// declaração normal
+// declare interface ButtonProps {
+
+
+// Modelo de tipagem por dicionário
+type WithChildren<T = {}> = 
+  T & { children?: React.ReactNode };
+
+type ButtonProps = WithChildren<{
     content?: string,
     onClick?: () => void,
     appendIcon: JSX.Element
-}
+}>
+
+
 
 
 /* 
 props.content  quando é passad o valor como atributo, exemplo: <Button content="teste"/>
 children quando é passado entre o elemento hml, exemplo: <Button>teste</Button>
 */
+
+/*
+Parametros ficarem visíveis em formato de função
 const Button: React.FC<ButtonProps>= (props) => {
+*/
+
+
+const Button = ({ content, appendIcon, onClick, children }: ButtonProps)  => {
     return <button className="AppButton" 
-    onClick={props.onClick}>
-        {props.children || 'NameLess button'}
-        {props.appendIcon }
+    onClick={ onClick}>
+        {children || 'NameLess button'}
+        {appendIcon }
     </button>
 }
 
