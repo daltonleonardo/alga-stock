0b279435d3da4eaf034070aed15f658e39f38500
dalton.leonardo <daltonleonardo@hotmail.com>
2020-11-13 21:59:31 -0300

    list render

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,23 +1,30 @@
 import React, { useState } from 'react';
-import Button from '../../shared/Button';
+// import Button from '../../shared/Button';
 import Container from '../../shared/Container';
-import Input from '../../shared/input';
+// import Input from '../../shared/input';
 import Header from '../Header';
-import './App.css';
+// import './App.css';
 
-function TestComponent() {
-  return <img width="16" src="http://img.icons8.com/pastel-glyph/2x/search--v2.png" alt="search icon" />
-}
+// function TestComponent() {
+//   return <img width="16" src="http://img.icons8.com/pastel-glyph/2x/search--v2.png" alt="search icon" />
+// }
 
 function App() {
 
-  const [street, setStreet] = useState('');
+  // const [street, setStreet] = useState('');
 
   return (
     <div className="App">
       <Header title="AlgaStock"></Header>
       <Container>
-        <Button content="Click me" onClick={window.alert}
+        <ul>
+        {
+        ['Daniel', 'Wiliiam', 'Thiago', 'Daniel'].map((name, index) => {
+          return <li key={index}>
+            { name }
+          </li>
+        })
+        /* <Button content="Click me" onClick={window.alert}
         appendIcon={<TestComponent/>}>
           Alert
          </Button>
@@ -26,7 +33,8 @@ function App() {
           placeholder="E.g.: 15h Avenue"
           value={street}
           onChange={e => setStreet(e.target.value)}
-        />
+        /> */}
+        </ul>
       </Container>
     </div>
   );
