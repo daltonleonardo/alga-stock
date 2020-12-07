582a3d3a855d8f0a760faa745393b46d69a06539
dalton.leonardo <daltonleonardo@hotmail.com>
2020-12-07 18:13:24 -0300

    insertNewProduct action dispatch by component

--- src/components/Products/ProductsCRUD.tsx
+++ src/components/Products/ProductsCRUD.tsx
@@ -8,7 +8,8 @@ import {
 import { Product } from '../../shared/Table/Table.mockdata'
 import ProductForm, { ProductCreator } from './ProductForm'
 import Swal from 'sweetalert2'
-import { connect } from 'react-redux'
+import { connect, useDispatch } from 'react-redux'
+import { insertNewProduct } from '../../redux/Products/Products.action'
 
 const headers: TableHeader[] = [
   { key: 'id', value: '#' },
@@ -22,6 +23,7 @@ declare interface ProductsCRUDProps {
 }
 
 const ProductsCRUD: React.FC<ProductsCRUDProps> = (props) => {
+ const dispatch = useDispatch();
   // const [products, setProducts] = useState<Product[]>([])
   const [updatingProduct, setUpdatingProduct] = useState<Product | undefined>(undefined)
 
@@ -36,7 +38,8 @@ const ProductsCRUD: React.FC<ProductsCRUDProps> = (props) => {
   
   const handleProductSubmit = async (product: ProductCreator) => {
     try {
-      await createSingleProduct(product)
+    //   await createSingleProduct(product)
+      dispatch(insertNewProduct(product));
       fetchData()
     } catch (err) {
       Swal.fire('Oops!', err.message, 'error')
