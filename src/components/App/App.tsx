f117ca1c5a0e319ca52cc8c1123426707be4df01
dalton.leonardo <daltonleonardo@hotmail.com>
2020-12-09 10:09:51 -0300

    add home view + react-router-dom

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,23 +1,19 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import './App.css';
-import Header from '../Header';
-import Container from '../../shared/Container';
-import ProductsCRUD from '../Products/ProductsCRUD';
-
+import HomeView from '../../views/HomeView';
 
 
 function App() {
  
-
- 
-  
-
   return (
     <div className="App">
-      <Header title="AlgaStock" />
-      <Container>
-        <ProductsCRUD/>
-      </Container>
+      <BrowserRouter>
+         <Switch>
+           <Route path="/" exact component={HomeView}>
+           </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
